7c278c4699bdede4b3a92d40f1b7ef283e89baeb
Benjamin Dinh <benjadinh@gmail.com>
2022-02-23 21:25:53 -0800

    Update ResizableArrayBagTest.java

--- Project 1 Code/src/ResizableArrayBagTest.java
+++ Project 1 Code/src/ResizableArrayBagTest.java
@@ -46,6 +46,59 @@ public class ResizableArrayBagTest
 
 
 
+        //Array bag test for difference
+      //Normal Case - equal size arrays
+      System.out.println("Difference general use case where arrays are same size");
+      BagInterface<String> diffBag1 = new ResizableArrayBag<>();//Initializing bag1 as a resizeable array bag.
+      System.out.println("Displaying the contents of bag1");
+      String[] a = {"A", "B", "C"};
+      testAdd(diffBag1, a);// Adding the string a to bag1
+      System.out.println("\nDisplaying the contents of bag2");
+
+      BagInterface<String> diffBag2 = new ResizableArrayBag<>();
+      String[] b = {"A", "C", "D"};
+      testAdd(diffBag2, b);
+
+      BagInterface<String> diffBag3 = diffBag1.difference(diffBag2); //Expected output is B
+      System.out.println("\nDisplaying the difference between the two");
+
+      displayBag(diffBag3);
+  
+
+      //Test Case different Size Arrays - Bigger B1
+      System.out.println("\nDifference general use case where arrays are different sizes");
+      System.out.println("\nDisplaying the contents of bag1 \n");
+      String[] c = {"A", "B"};
+      testAdd(diffBag1, a);//db1 Should be A B C A B
+
+      System.out.println("\nDisplaying the contents of bag2 \n");
+      displayBag(diffBag2);//db2 should be A C D
+      diffBag3 = diffBag1.difference(diffBag2); //Expected output is B A B
+      System.out.println("\nDisplaying the difference between bag1 and bag2\n");
+
+      displayBag(diffBag3);
+
+
+      System.out.println("\nDisplaying the difference between bag2 and bag1\n");
+      diffBag3 = diffBag2.difference(diffBag1); 
+      displayBag(diffBag3); //Bigger B2
+
+      BagInterface<String> diffBag4 = new ResizableArrayBag<>();
+      BagInterface<String> diffBag5 = new ResizableArrayBag<>();
+      System.out.println("\nOutputting difference between empty array and normal array");
+      diffBag3 = diffBag4.difference(diffBag2);
+      displayBag(diffBag3);//Output should be a nothing due to array being empty
+
+      System.out.println("\nOutputting difference normal array and empty array");
+      diffBag3 = diffBag2.difference(diffBag4);
+      displayBag(diffBag3);//Output should be the contents of B2
+      
+      System.out.println("\nOutputting difference empty array and empty array");
+      diffBag3 = diffBag4.difference(diffBag5);
+      displayBag(diffBag3);//Output should be the contents of B2
+
+
+
 
         // Here we will test the three methods
 //        String[] bag1 = {"a", "b", "c"};
